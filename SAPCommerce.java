488d373b76c8a5a153004db2f8b308b6dceb3a95
buba-vms <victor.buba@live.com>
2023-01-16 10:57:11 -0300

    first commit

---
+++ SAPCommerce.java
@@ -0,0 +1,10 @@
+public class SAPCommerce {
+    public void addToCart(){
+        System.out.println("Item adicionado ao carrinho");
+    }
+
+    public void removeItemFromCart(){
+        System.out.println("Item removido do carrinho");
+    }
+
+}
